048cb86572edde7ba8394c69fa4af194831b2dd0
Mikhail-Lytov <Misha.Lytov@yandex.ru>
2022-10-02 20:27:31 +0300

    bubble_sorting

--- src/simple_sort.java
+++ src/simple_sort.java
@@ -17,94 +17,147 @@ public class simple_sort<E extends Comparable<E>> implements Comparable<E>  {
     public  simple_sort(){
         //this.arr_object = arr;
     }
-    public Object[] bubble_sorting(Object[] arr){
+   /* private void interpret(Object[] arr){
         this.arr_object = arr;
         int size = arr.length;
         this.arr_object = Arrays.copyOf(arr_object,size);
-        bubble_sorting_object();
-        return arr_object;
-    }
-    public int[] bubble_sorting(int[] arr){
+    }*/
+    private void interpret(int[] arr){
         this.arr_int = arr;
         int size = arr.length;
         this.arr_object = Arrays.copyOf(arr_object,size);
         for(int i = 0; i < size; i++){
             this.arr_object[i] = (int)arr[i];
         }
-        bubble_sorting_object();
-        for (int i = 0; i < size; i++){
-            arr_int[i] = (int)arr_object[i];
-        }
-        return arr_int;
     }
-    public char[] bubble_sorting(char[] arr){
+    private void interpret(char[] arr){
         this.arr_char = arr;
         int size = arr.length;
         this.arr_object = Arrays.copyOf(arr_object,size);
         for(int i = 0; i < size; i++){
             this.arr_object[i] = (char)arr[i];
         }
-        bubble_sorting_object();
-        for (int i = 0; i < size; i++){
-            arr_char[i] = (char)arr_object[i];
-        }
-        return arr_char;
     }
-    public String[] bubble_sorting(String[] arr){
+    private void interpret(String[] arr){
         this.arr_string = arr;
         int size = arr.length;
         this.arr_object = Arrays.copyOf(arr_object,size);
         for(int i = 0; i < size; i++){
             this.arr_object[i] = (String)arr[i];
         }
-        bubble_sorting_object();
-        for (int i = 0; i < size; i++){
-            arr_string[i] = (String) arr_object[i];
-        }
-        return arr_string;
     }
-    public double[] bubble_sorting(double[] arr){
+    private void interpret(double[] arr){
         this.arr_double = arr;
         int size = arr.length;
         this.arr_object = Arrays.copyOf(arr_object,size);
         for(int i = 0; i < size; i++){
             this.arr_object[i] = (double)arr[i];
         }
-        bubble_sorting_object();
-        for (int i = 0; i < size; i++){
+    }
+    public Object[] bubble_sorting(Object[] arr){
+        //interpret(arr);
+        bubble_sorting_object(0,arr_object.length);
+        return arr_object;
+    }
+    public Object[] bubble_sorting(Object[] arr, int first, int second){
+        bubble_sorting_object(first,second);
+        return arr_object;
+    }
+    public int[] bubble_sorting(int[] arr){
+        interpret(arr);
+        bubble_sorting_object(0,arr_object.length);
+        for (int i = 0; i < arr_object.length; i++){
+            arr_int[i] = (int)arr_object[i];
+        }
+        return arr_int;
+    }
+    public int[] bubble_sorting(int[] arr, int first, int second){
+        interpret(arr);
+        bubble_sorting_object(first,second);
+        for (int i = 0; i < arr_object.length; i++){
+            arr_int[i] = (int)arr_object[i];
+        }
+        return arr_int;
+    }
+    public char[] bubble_sorting(char[] arr){
+        interpret(arr);
+        bubble_sorting_object(0,arr_object.length);
+        for (int i = 0; i < arr_object.length; i++){
+            arr_char[i] = (char)arr_object[i];
+        }
+        return arr_char;
+    }
+    public char[] bubble_sorting(char[] arr, int first, int second) {
+        interpret(arr);
+        bubble_sorting_object(first, second);
+        for (int i = 0; i < arr_object.length; i++) {
+            arr_char[i] = (char) arr_object[i];
+        }
+        return arr_char;
+    }
+    public String[] bubble_sorting(String[] arr){
+        interpret(arr);
+        bubble_sorting_object(0,arr_object.length);
+        for (int i = 0; i < arr_object.length; i++){
+            arr_string[i] = (String) arr_object[i];
+        }
+        return arr_string;
+    }
+    public String[] bubble_sorting(String[] arr, int first, int second) {
+        interpret(arr);
+        bubble_sorting_object(first, second);
+        for (int i = 0; i < arr_object.length; i++) {
+            arr_string[i] = (String) arr_object[i];
+        }
+        return arr_string;
+    }
+    public double[] bubble_sorting(double[] arr){
+        interpret(arr);
+        bubble_sorting_object(0,arr_object.length);
+        for (int i = 0; i < arr_object.length; i++){
             arr_double[i] = (double)arr_object[i];
         }
         return arr_double;
     }
+    public double[] bubble_sorting(double[] arr, int first, int second) {
+        interpret(arr);
+        bubble_sorting_object(first, second);
+        for (int i = 0; i < arr_object.length; i++) {
+            arr_double[i] = (Double) arr_object[i];
+        }
+        return arr_double;
+    }
 
-    private void bubble_sorting_object(){
+    private void bubble_sorting_object(int first, int second){
         //Object a = "1";
         //om = (E) a;
         int result ;
         Object variable;
         boolean flag = true;
         int size = arr_object.length;
-        //arr_int = Arrays.copyOf(arr_int,size);
-        while (flag) {
-            flag = false;
-            for (int i = 0; i < (size - 1); i++) {
-                item = (E) arr_object[i];
-                item_2 = (E) arr_object[i + 1];
-                result = compareTo(item_2);
-                if (result > 0) {
-                    variable = arr_object[i + 1];
-                    arr_object[i + 1] = arr_object[i];
-                    arr_object[i] = variable;
-                    flag = true;
+        try {
+            //arr_int = Arrays.copyOf(arr_int,size);
+            while (flag) {
+                flag = false;
+                for (int i = first; i < (size - 1) && i < (second); i++) {
+                    item = (E) arr_object[i];
+                    item_2 = (E) arr_object[i + 1];
+                    result = compareTo(item_2);
+                    if (result > 0) {
+                        variable = arr_object[i + 1];
+                        arr_object[i + 1] = arr_object[i];
+                        arr_object[i] = variable;
+                        flag = true;
+                    }
                 }
             }
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("index error");
+        }catch (ArrayStoreException e){
+            System.out.println(e.getMessage());
         }
         //System.out.println(Arrays.toString(arr_object));
     }
-    private int[] bubble_sorting_object(int first, int second){
-        return arr_int;
-    }
-
     @Override
     public int compareTo(E o) {
         int result = this.item.compareTo(o);
--- src/test_simple_sort.java
+++ src/test_simple_sort.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class test_simple_sort<E> {
     public static void main(String[] args) {
-        String[] arr = {"privet", "my", "names"};
+        int[] arr = {161,48,141,94,51,106,3,167,50,152,67,116,57,157,155,8,39,114,74,107,146,125,158,12,181,161,129,119,37,30,88,111,106,55,76,60,58,108,144,108,81,187,5,210,150,20,61,111,66,28};
         simple_sort sort = new simple_sort() ;
         arr = sort.bubble_sorting(arr);
-        System.out.println(Arrays.toString(sort.bubble_sorting(arr)));
+        System.out.println(Arrays.toString(arr));
     }
 }
