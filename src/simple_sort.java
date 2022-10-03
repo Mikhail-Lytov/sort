000b1f5808ebf02bfc8d171ceb3d2c16b22ee3f9
Mikhail-Lytov <Misha.Lytov@yandex.ru>
2022-10-03 17:42:09 +0300

    +sorting_by_inserts

--- src/simple_sort.java
+++ src/simple_sort.java
@@ -6,22 +6,14 @@ import javax.swing.JButton;
 public class simple_sort<E extends Comparable<E>> implements Comparable<E>  {
     private E item;
     private E item_2;
-    int[] arr_int = new int[1];
-    char[] arr_char = new char[1];
-    double[] arr_double = new double[1];
-    String[] arr_string = new String[1];
-    Object[] arr_object = new Object[1];
-    //public simple_sort(){
+    private int[] arr_int = new int[1];
+    private char[] arr_char = new char[1];
+    private double[] arr_double = new double[1];
+    private String[] arr_string = new String[1];
+    private Object[] arr_object = new Object[1];
 
-    //}
-    public  simple_sort(){
-        //this.arr_object = arr;
-    }
-   /* private void interpret(Object[] arr){
-        this.arr_object = arr;
-        int size = arr.length;
-        this.arr_object = Arrays.copyOf(arr_object,size);
-    }*/
+    public  simple_sort(){}
+
     private void interpret(int[] arr){
         this.arr_int = arr;
         int size = arr.length;
@@ -54,77 +46,145 @@ public class simple_sort<E extends Comparable<E>> implements Comparable<E>  {
             this.arr_object[i] = (double)arr[i];
         }
     }
+
+    private void return_int(){
+        for (int i = 0; i < arr_object.length; i++){
+            arr_int[i] = (int)arr_object[i];
+        }
+    }
+    private void return_char(){
+        for (int i = 0; i < arr_object.length; i++){
+            arr_char[i] = (char)arr_object[i];
+        }
+    }
+    private void return_String(){
+        for (int i = 0; i < arr_object.length; i++) {
+            arr_string[i] = (String) arr_object[i];
+        }
+    }
+    private void return_double(){
+        for (int i = 0; i < arr_object.length; i++){
+            arr_double[i] = (double)arr_object[i];
+        }
+    }
+    public Object[] sorting_by_inserts(Object[] arr){
+        this.arr_object = arr;
+        sorting_by_inserts_object(0,arr_object.length);
+        return arr_object;
+    }
+    public Object[] sorting_by_inserts(Object[] arr, int first, int prev){
+        this.arr_object = arr;
+        sorting_by_inserts_object(first,prev);
+        return arr_object;
+    }
     public Object[] bubble_sorting(Object[] arr){
-        //interpret(arr);
+        this.arr_object = arr;
         bubble_sorting_object(0,arr_object.length);
         return arr_object;
     }
     public Object[] bubble_sorting(Object[] arr, int first, int second){
+        this.arr_object = arr;
         bubble_sorting_object(first,second);
         return arr_object;
+    }
+
+    public int[] sorting_by_inserts(int[] arr, int first, int second){
+        interpret(arr);
+        sorting_by_inserts_object(first, second);
+        return_int();
+        return arr_int;
+    }
+    public int[] sorting_by_inserts(int[] arr){
+        interpret(arr);
+        sorting_by_inserts_object(0,arr_object.length);
+        return_int();
+        return arr_int;
     }
     public int[] bubble_sorting(int[] arr){
         interpret(arr);
         bubble_sorting_object(0,arr_object.length);
-        for (int i = 0; i < arr_object.length; i++){
-            arr_int[i] = (int)arr_object[i];
-        }
+        return_int();
         return arr_int;
     }
     public int[] bubble_sorting(int[] arr, int first, int second){
         interpret(arr);
         bubble_sorting_object(first,second);
-        for (int i = 0; i < arr_object.length; i++){
-            arr_int[i] = (int)arr_object[i];
-        }
+        return_int();
         return arr_int;
+    }
+
+    public char[] sorting_by_inserts(char[] arr, int first, int second){
+        interpret(arr);
+        sorting_by_inserts_object(first, second);
+        return_char();
+        return arr_char;
+    }
+    public char[] sorting_by_inserts(char[] arr){
+        interpret(arr);
+        sorting_by_inserts_object(0,arr_object.length);
+        return_char();
+        return arr_char;
     }
     public char[] bubble_sorting(char[] arr){
         interpret(arr);
         bubble_sorting_object(0,arr_object.length);
-        for (int i = 0; i < arr_object.length; i++){
-            arr_char[i] = (char)arr_object[i];
-        }
+        return_char();
         return arr_char;
     }
     public char[] bubble_sorting(char[] arr, int first, int second) {
         interpret(arr);
         bubble_sorting_object(first, second);
-        for (int i = 0; i < arr_object.length; i++) {
-            arr_char[i] = (char) arr_object[i];
-        }
+        return_char();
         return arr_char;
+    }
+
+    public String[] sorting_by_inserts(String[] arr, int first, int second){
+        interpret(arr);
+        sorting_by_inserts_object(first,second);
+        return_String();
+        return arr_string;
+    }
+    public String[] sorting_by_inserts(String[] arr){
+        interpret(arr);
+        sorting_by_inserts_object(0,arr_object.length);
+        return_String();
+        return arr_string;
     }
     public String[] bubble_sorting(String[] arr){
         interpret(arr);
         bubble_sorting_object(0,arr_object.length);
-        for (int i = 0; i < arr_object.length; i++){
-            arr_string[i] = (String) arr_object[i];
-        }
+        return_String();
         return arr_string;
     }
     public String[] bubble_sorting(String[] arr, int first, int second) {
         interpret(arr);
         bubble_sorting_object(first, second);
-        for (int i = 0; i < arr_object.length; i++) {
-            arr_string[i] = (String) arr_object[i];
-        }
+        return_String();
         return arr_string;
+    }
+
+    public double[] sorting_by_inserts(double[] arr){
+        interpret(arr);
+        sorting_by_inserts_object(0, arr_object.length);
+        return_double();
+        return arr_double;
+    }
+    public double[] sorting_by_inserts(double[] arr, int first, int second){
+        interpret(arr);
+        sorting_by_inserts_object(second, first);
+        return_double();
+        return arr_double;
     }
     public double[] bubble_sorting(double[] arr){
         interpret(arr);
         bubble_sorting_object(0,arr_object.length);
-        for (int i = 0; i < arr_object.length; i++){
-            arr_double[i] = (double)arr_object[i];
-        }
+        return_double();
         return arr_double;
     }
     public double[] bubble_sorting(double[] arr, int first, int second) {
         interpret(arr);
         bubble_sorting_object(first, second);
-        for (int i = 0; i < arr_object.length; i++) {
-            arr_double[i] = (Double) arr_object[i];
-        }
+        return_double();
         return arr_double;
     }
 
@@ -139,7 +199,7 @@ public class simple_sort<E extends Comparable<E>> implements Comparable<E>  {
             //arr_int = Arrays.copyOf(arr_int,size);
             while (flag) {
                 flag = false;
-                for (int i = first; i < (size - 1) && i < (second); i++) {
+                for (int i = first; i < (size - 1) && i < (second - 1); i++) {
                     item = (E) arr_object[i];
                     item_2 = (E) arr_object[i + 1];
                     result = compareTo(item_2);
@@ -157,6 +217,59 @@ public class simple_sort<E extends Comparable<E>> implements Comparable<E>  {
             System.out.println(e.getMessage());
         }
         //System.out.println(Arrays.toString(arr_object));
+    }
+    private void sorting_by_inserts_object(int first, int second){
+        boolean flag = true;
+        int result;
+        Object variable;
+        int gap;
+        for (int i = first + 1; i < second - 1; i++){
+            item = (E) arr_object[i];
+            item_2 = (E) arr_object[i-1];
+            result = compareTo(item_2);
+            if(result < 0){
+                variable = arr_object[i];
+                gap = i;
+                for (int j = i - 1; j > first - 1; j--){
+                    item_2 = (E) arr_object[j];
+                    result = compareTo(item_2);
+                    if(result < 0){
+                        arr_object[gap] = arr_object[j];
+                        if(j == first){
+                            arr_object[j] = variable;
+                        }
+                        gap--;
+                    } else if (result >= 0) {
+                        arr_object[gap] = variable;
+                        break;
+                    }
+                }
+            }
+        }
+        for (int i = second - 1; i > first; i--){
+            item = (E) arr_object[i];
+            item_2 = (E) arr_object[i - 1];
+            result = compareTo(item_2);
+            if(result < 0){
+                variable = arr_object[i];
+                gap = i;
+                for (int j = i - 1; j > first - 1;j--){
+                    item_2 = (E) arr_object[j];
+                    result = compareTo(item_2);
+                    if(result < 0){
+                        arr_object[gap] = arr_object[j];
+                        if(j == first){
+                            arr_object[j] = variable;
+                        }
+                        gap--;
+                    } else if (result >= 0) {
+                        arr_object[gap] = variable;
+                        break;
+                    }
+                }
+            }
+
+        }
     }
     @Override
     public int compareTo(E o) {
--- src/test_simple_sort.java
+++ src/test_simple_sort.java
@@ -2,9 +2,14 @@ import java.util.Arrays;
 
 public class test_simple_sort<E> {
     public static void main(String[] args) {
-        int[] arr = {161,48,141,94,51,106,3,167,50,152,67,116,57,157,155,8,39,114,74,107,146,125,158,12,181,161,129,119,37,30,88,111,106,55,76,60,58,108,144,108,81,187,5,210,150,20,61,111,66,28};
+        int[] arr = {4,6,5,12,2,3,8,13,7,1,11,16,10,14,15,9};
+        System.out.println(Arrays.toString(arr));
         simple_sort sort = new simple_sort() ;
-        arr = sort.bubble_sorting(arr);
+        //arr = sort.bubble_sorting(arr);
+        int[] arr_2 = {4,6,5,12,2,3,8,13,7,1,11,16,10,14,15,9};
+        arr_2 = sort.bubble_sorting(arr_2);
+        System.out.println(Arrays.toString(arr_2));
+        arr = sort.sorting_by_inserts(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
